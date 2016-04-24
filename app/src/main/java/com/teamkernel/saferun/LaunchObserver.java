1f6de6e646d1b8e4e72a561b9710657bd92b1b55
Angela Fox <angelafox92@gmail.com>
2016-04-23 23:06:44 -0400

    Added Firebase dependency

--- app/src/main/java/com/teamkernel/saferun/LaunchObserver.java
+++ app/src/main/java/com/teamkernel/saferun/LaunchObserver.java
@@ -3,6 +3,7 @@ package com.teamkernel.saferun;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,5 +13,15 @@ public class LaunchObserver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_observer);
+    }
+
+    public void observerJoinRun(View view){
+        Intent intent = new Intent(this, ObserverInPosition.class);
+        startActivity(intent);
+    }
+
+    public void observerGoBackToMain(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
