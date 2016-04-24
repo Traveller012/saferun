f285100c68969946ee8265827d6e537e60130af0
Angela Fox <angelafox92@gmail.com>
2016-04-23 22:47:37 -0400

    Fixed bug with Driver Map View

--- app/src/main/java/com/teamkernel/saferun/LaunchObserver.java
+++ app/src/main/java/com/teamkernel/saferun/LaunchObserver.java
@@ -12,13 +12,5 @@ public class LaunchObserver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_observer);
-        Intent intent = getIntent();
-        String username = intent.getStringExtra(MainActivity.EXTRA_USERNAME);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(username);
-
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.launchObserver);
-        layout.addView(textView);
     }
 }
