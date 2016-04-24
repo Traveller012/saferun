d95ea0eac53a3b98353896893f6ed60cee93ce9f
Angela Fox <angelafox92@gmail.com>
2016-04-23 22:37:12 -0400

    Basic functionality for Driver created

--- app/src/main/java/com/teamkernel/saferun/LaunchDriver.java
+++ app/src/main/java/com/teamkernel/saferun/LaunchDriver.java
@@ -3,6 +3,7 @@ package com.teamkernel.saferun;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,13 +13,16 @@ public class LaunchDriver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_driver);
-        Intent intent = getIntent();
-        String username = intent.getStringExtra(MainActivity.EXTRA_USERNAME);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(username);
 
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.launchDriver);
-        layout.addView(textView);
+    }
+    public void driverJoinRun(View view){
+        Intent intent = new Intent(this, DriverMapView.class);
+        startActivity(intent);
+    }
+
+    public void driverGoBackToMain(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
+
